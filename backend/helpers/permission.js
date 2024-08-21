f19561a1334a1da0d61e382c018c2860b04cac0c
Lakmini-2001 <lakminir2001@gmail.com>
2024-08-21 12:08:22 +0530

    Prodduct upload permission (updated)

--- backend/helpers/permission.js
+++ backend/helpers/permission.js
@@ -1,6 +1,14 @@
 const userModel = require("../models/userModel")
 
-const uploadProductPermission = async(userId) => {}
+const uploadProductPermission = async(userId) => {
+    const user = await userModel.findById(userId)
+
+    if(user.role === 'ADMIN'){
+        return true
+    }
+
+    return false
+}
 
 
 module.exports = uploadProductPermission
