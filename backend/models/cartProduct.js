fe8f62ead80c7f0db06ab4e9623adee53e8fdd13
Lakmini-2001 <lakminir2001@gmail.com>
2024-08-22 10:23:29 +0530

    Models created (cart, product, user)

--- backend/models/cartProduct.js
+++ backend/models/cartProduct.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose')
 
-const addToCart = mongoose.Schema({
-   productId : {
-        ref : 'product',
-        type : String,
-   },
-   quantity : Number,
-   userId : String,
-},{
-    timestamps : true
-})
-
-
-const addToCartModel = mongoose.model("addToCart",addToCart)
+const addToCart = mongoose.Schema({})
 
 module.exports = addToCartModel
